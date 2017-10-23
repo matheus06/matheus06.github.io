64d5ebce985cf67996a65029060a7292be878652
Matheus Sexto <matheus.sexto@gmail.com>
2017-10-22 22:14:21 -0200

    Add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+//bootstrap
+import {CollapseModule} from 'ngx-bootstrap/collapse'
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
